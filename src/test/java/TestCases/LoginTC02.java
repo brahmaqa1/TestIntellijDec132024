9cb49b85c2584605ed208306c0dbc446a071a02c
brahmaqa1 <brahma.qa1@gmail.com>
2025-04-17 01:43:32 +0530

    Update LoginTC02.java

--- src/test/java/TestCases/LoginTC02.java
+++ src/test/java/TestCases/LoginTC02.java
@@ -11,7 +11,7 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTC02 {
-// add new test 2
+// add new test 2 17 apr 
     @Test
     public void loginTc01() {
         // open browser
